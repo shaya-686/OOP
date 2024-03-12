9513cb379d07ffa26e3bf77966af94dd5e873c49
shaya-686 <oleksandra.bogdanchenko@gmail.com>
2024-03-12 19:46:40 +0200

    Dogs class initialization

--- main.py
+++ main.py
@@ -21,10 +21,6 @@ class Cat(Animals):
     pass
 
 
-# my_dog = Dogs()
-# my_dog.breath()
-# my_dog.move()
-# my_dog.eat_food()
 my_dog = Dogs("Bob", 5, "labrador")
 my_dog_2 = Dogs("Em", 6, "labrador")
 print(my_dog.name)
