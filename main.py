9ec2513f506b1df5c9c41bcd99c8db8756aa1e43
shaya-686 <oleksandra.bogdanchenko@gmail.com>
2024-03-12 19:39:17 +0200

    Animals class imitation

--- main.py
+++ main.py
@@ -11,14 +11,22 @@ class Animals:
 
 
 class Dogs(Animals):
-    pass
+    def __init__(self, name, age, breed):
+        self.name = name
+        self.age = age
+        self.breed = breed
 
 
 class Cat(Animals):
     pass
 
 
-my_dog = Dogs()
-my_dog.breath()
-my_dog.move()
-my_dog.eat_food()
+# my_dog = Dogs()
+# my_dog.breath()
+# my_dog.move()
+# my_dog.eat_food()
+my_dog = Dogs("Bob", 5, "labrador")
+my_dog_2 = Dogs("Em", 6, "labrador")
+print(my_dog.name)
+print(my_dog.age)
+print(my_dog.breed)
