93e0882070794d98482cc354453356a40008a5f1
shaya-686 <oleksandra.bogdanchenko@gmail.com>
2024-03-14 20:34:00 +0200

    Class Stadium with overloading operators and methods

--- main.py
+++ main.py
@@ -1,54 +1,67 @@
-# Створіть клас «Дріб». Збережіть у класі чисельник
-# та знаменник. Реалізуйте методи класу для введеннявиведення даних. Також створіть методи класу для
-# виконання арифметичних операцій (додавання,
-# віднімання, множення, ділення і т. д.). До вже
-# реалізованого класу «Дріб» додайте необхідні
-# перевантажені методи та оператори.
+# Реалізуйте клас «Стадіон». Збережіть у класі: назву
+# стадіону, дату відкриття, країну, місто, місткість. Реалізуйте
+# методи класу для введення-виведення даних та інших
+# операцій. До вже реалізованого класу «Стадіон» додайте
+# необхідні перевантажені методи та оператори.
 
 
-class Fraction:
-    def __init__(self, nominator, denominator):
-        self.nominator = nominator
-        self.denominator = denominator
+class Stadium:
+    def __init__(self, name, date_opening, country, city, capacity):
+        self.name = name
+        self.date_opening = date_opening
+        self.country = country
+        self.city = city
+        self.capacity = capacity
 
     def __str__(self):
-        return f"{self.nominator}/{self.denominator}"
+        return (f"Stadium information: "
+                f"name - {self.name}, "
+                f"date of opening - {self.date_opening},"
+                f"country - {self.country},"
+                f"city - {self.city},"
+                f"capacity - {self.capacity}")
 
-    def __add__(self, other):
-        result_nominator = self.nominator * other.denominator + other.nominator * self.denominator
-        result_denominator = other.denominator * self.denominator
-        return Fraction(result_nominator, result_denominator)
+    def __add__(self, cap):
+        self.capacity += cap
 
-    def __sub__(self, other):
-        result_nominator = self.nominator * other.denominator - other.nominator * self.denominator
-        result_denominator = other.denominator * self.denominator
-        return Fraction(result_nominator, result_denominator)
+    def __sub__(self, cap):
+        if self.capacity < cap:
+            print("Stadium capacity is less then capacity that you provide")
+        else:
+            self.capacity -= cap
+            return self
 
-    def __mul__(self, other):
-        result_nominator = self.nominator * other.nominator
-        result_denominator = other.denominator * self.denominator
-        return Fraction(result_nominator, result_denominator)
+    def __eq__(self, other):
+        if self.capacity == other.capacity:
+            return True
+        else:
+            return False
 
-    def __truediv__(self, other):
-        result_nominator = self.nominator * other.denominator
-        result_denominator = other.nominator * self.denominator
-        return Fraction(result_nominator, result_denominator)
+    def __gt__(self, other):
+        if self.capacity > other.capacity:
+            return True
+        else:
+            return False
+
+    def __lt__(self, other):
+        if self.capacity < other.capacity:
+            return True
+        else:
+            return False
 
 
-first_fraction = Fraction(10, 20)
-new_fraction = Fraction(5, 20)
-print(first_fraction)
-print(new_fraction)
+stadium = Stadium("Olymp", "15.02.2000", "Ukraine", "Kyiv", 100000)
+stadium2 = Stadium("Olympus", "15.02.2000", "Ukraine", "Kyiv", 99800)
+print(stadium)
 
-
-result_addition = first_fraction + new_fraction
-print(result_addition)
-
-result_subtraction = first_fraction - new_fraction
-print(result_subtraction)
-
-result_multiplication = first_fraction * new_fraction
-print(result_multiplication)
-
-result_division = first_fraction/new_fraction
-print(result_division)
+stadium_with_increase_capacity = stadium + 100
+stadium_with_reduced_capacity = stadium - 500
+print(stadium_with_increase_capacity, stadium_with_reduced_capacity)
+if stadium == stadium2:
+    print("Capacity of this stadiums is the same")
+elif stadium > stadium2:
+    print("First stadium is bigger then the second stadium")
+elif stadium < stadium2:
+    print("First stadium is less then the second stadium")
+else:
+    print("Capacity of this stadiums is not equal")
