b6b0e1d32d65411192b092c119cbb2e98b4aae11
shaya-686 <oleksandra.bogdanchenko@gmail.com>
2024-03-14 19:57:17 +0200

    Class Fraction with overloading operators and methods

--- main.py
+++ main.py
@@ -1,14 +1,54 @@
-# Operator overloading
-class ComplexNumber:
-    def __init__(self, real, imag):
-        self.real = real
-        self.imag = imag
+# Створіть клас «Дріб». Збережіть у класі чисельник
+# та знаменник. Реалізуйте методи класу для введеннявиведення даних. Також створіть методи класу для
+# виконання арифметичних операцій (додавання,
+# віднімання, множення, ділення і т. д.). До вже
+# реалізованого класу «Дріб» додайте необхідні
+# перевантажені методи та оператори.
+
+
+class Fraction:
+    def __init__(self, nominator, denominator):
+        self.nominator = nominator
+        self.denominator = denominator
+
+    def __str__(self):
+        return f"{self.nominator}/{self.denominator}"
 
     def __add__(self, other):
-        return ComplexNumber(self.real + other.real, self.imag + other.imag)
+        result_nominator = self.nominator * other.denominator + other.nominator * self.denominator
+        result_denominator = other.denominator * self.denominator
+        return Fraction(result_nominator, result_denominator)
+
+    def __sub__(self, other):
+        result_nominator = self.nominator * other.denominator - other.nominator * self.denominator
+        result_denominator = other.denominator * self.denominator
+        return Fraction(result_nominator, result_denominator)
+
+    def __mul__(self, other):
+        result_nominator = self.nominator * other.nominator
+        result_denominator = other.denominator * self.denominator
+        return Fraction(result_nominator, result_denominator)
+
+    def __truediv__(self, other):
+        result_nominator = self.nominator * other.denominator
+        result_denominator = other.nominator * self.denominator
+        return Fraction(result_nominator, result_denominator)
 
 
-num1 = ComplexNumber(1, 2)
-num2 = ComplexNumber(3, 4)
-result = num1 + num2
-print(result)
+first_fraction = Fraction(10, 20)
+new_fraction = Fraction(5, 20)
+print(first_fraction)
+print(new_fraction)
+
+
+result_addition = first_fraction + new_fraction
+print(result_addition)
+
+result_subtraction = first_fraction - new_fraction
+print(result_subtraction)
+
+result_multiplication = first_fraction * new_fraction
+print(result_multiplication)
+
+result_division = first_fraction/new_fraction
+print(result_division)
