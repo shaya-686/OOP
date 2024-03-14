128fd5c684cfd031da1c4bab6c3681419d9db628
shaya-686 <oleksandra.bogdanchenko@gmail.com>
2024-03-14 19:37:22 +0200

    Operator overloading

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-# Method overloading
-class StringManipulator:
-    def concatenate(self, a, b):
-        return a + b
+# Operator overloading
+class ComplexNumber:
+    def __init__(self, real, imag):
+        self.real = real
+        self.imag = imag
 
-    def concatenate(self, a, b):
-        return str(a) + str(b)
+    def __add__(self, other):
+        return ComplexNumber(self.real + other.real, self.imag + other.imag)
 
 
-calc = StringManipulator()
-print(calc.concatenate(5, 5))
-print(calc.concatenate("5", "5"))
+num1 = ComplexNumber(1, 2)
+num2 = ComplexNumber(3, 4)
+result = num1 + num2
+print(result)
