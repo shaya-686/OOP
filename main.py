0688b77978e0dac08940e5e51a90b7dfc34286ab
shaya-686 <oleksandra.bogdanchenko@gmail.com>
2024-03-14 20:46:08 +0200

    Class Car with overloading operators and methods

--- main.py
+++ main.py
@@ -1,67 +1,37 @@
-# Реалізуйте клас «Стадіон». Збережіть у класі: назву
-# стадіону, дату відкриття, країну, місто, місткість. Реалізуйте
-# методи класу для введення-виведення даних та інших
-# операцій. До вже реалізованого класу «Стадіон» додайте
+# До вже реалізованого класу «Автомобіль» додайте
 # необхідні перевантажені методи та оператори.
 
-
-class Stadium:
-    def __init__(self, name, date_opening, country, city, capacity):
-        self.name = name
-        self.date_opening = date_opening
-        self.country = country
-        self.city = city
-        self.capacity = capacity
+class Car:
+    def __init__(self, brand, model, year):
+        self.brand = brand
+        self.model = model
+        self.year = year
 
     def __str__(self):
-        return (f"Stadium information: "
-                f"name - {self.name}, "
-                f"date of opening - {self.date_opening},"
-                f"country - {self.country},"
-                f"city - {self.city},"
-                f"capacity - {self.capacity}")
-
-    def __add__(self, cap):
-        self.capacity += cap
-
-    def __sub__(self, cap):
-        if self.capacity < cap:
-            print("Stadium capacity is less then capacity that you provide")
-        else:
-            self.capacity -= cap
-            return self
+        return (f"Car information: "
+                f"brand - {self.brand}, "
+                f"model - {self.model},"
+                f"year - {self.year}")
 
     def __eq__(self, other):
-        if self.capacity == other.capacity:
-            return True
-        else:
-            return False
+        return self.year == other.year
 
     def __gt__(self, other):
-        if self.capacity > other.capacity:
-            return True
-        else:
-            return False
+        return self.year > other.year
 
     def __lt__(self, other):
-        if self.capacity < other.capacity:
-            return True
-        else:
-            return False
+        return self.year < other.year
 
 
-stadium = Stadium("Olymp", "15.02.2000", "Ukraine", "Kyiv", 100000)
-stadium2 = Stadium("Olympus", "15.02.2000", "Ukraine", "Kyiv", 99800)
-print(stadium)
+car1 = Car("AUDI", "A6", "2022")
+car2 = Car("AUDI", "A6", "2020")
+print(car1)
 
-stadium_with_increase_capacity = stadium + 100
-stadium_with_reduced_capacity = stadium - 500
-print(stadium_with_increase_capacity, stadium_with_reduced_capacity)
-if stadium == stadium2:
-    print("Capacity of this stadiums is the same")
-elif stadium > stadium2:
-    print("First stadium is bigger then the second stadium")
-elif stadium < stadium2:
-    print("First stadium is less then the second stadium")
+if car1 == car2:
+    print("The years of this cars is the same")
+elif car1 > car2:
+    print("First car is newer then the second car")
+elif car1 < car2:
+    print("First car is older then the second car")
 else:
-    print("Capacity of this stadiums is not equal")
+    print("Year of this cars is not equal")
