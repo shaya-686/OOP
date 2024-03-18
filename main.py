e9736387e214a6679392ae2c67a701ff9b1eb627
shaya-686 <oleksandra.bogdanchenko@gmail.com>
2024-03-18 21:56:28 +0200

    Class Website with overloading operators

--- main.py
+++ main.py
@@ -1,71 +1,54 @@
-class City:
-    def __init__(self, name, region, country, population, zipCode, telephoneCode):
+class Website:
+    def __init__(self, name, siteURL, description):
         self.name = name
-        self.region = region
-        self.country = country
-        self.population = population
-        self.zipCode = zipCode
-        self.telephoneCode = telephoneCode
+        self.siteURL = siteURL
+        self.description = description
 
-    def create_city(self):
-        print("Enter the information about the city: ")
-        self.name = input("Enter the city name: ")
-        self.region = input("Enter the city region: ")
-        self.country = input("Enter the city country: ")
-        while True:
-            try:
-                self.population = int(input("Enter the city population: "))
-                break
-            except ValueError as e:
-                print("Population should be int")
-        self.zipCode = input("Enter the city zipCode: ")
-        self.telephoneCode = input("Enter the city telephoneCode: ")
+    def create_site(self):
+        print("Enter the information about the site: ")
+        self.name = input("Enter the site name: ")
+        self.siteURL = input("Enter the siteURL: ")
+        self.description = input("Enter the site description: ")
 
     def __str__(self):
-        return (f"Information about the city: "
+        return (f"Information about the site: "
                 f"name - {self.name}, "
-                f"region - {self.region}, "
-                f"population - {self.population},"
-                f"telephoneCode - {self.telephoneCode},"
-                f"country - {self.country},"
-                f"zipCode - {self.zipCode}")
+                f"URL - {self.siteURL}, "
+                f"description - {self.description}")
 
-    def __add__(self, some_population):
-        self.population += some_population
+    def __add__(self, descr):
+        self.description += " " + descr
         return self
 
-    def __sub__(self, some_population):
-        if some_population > self.population:
-            print("Population cannot be changed")
-        else:
-            self.population -= some_population
-            return self
+    def __sub__(self, descr):
+        self.description = self.description.replace(descr, "")
+        return self
 
     def __eq__(self, other):
-        return self.population == other.population
+        return len(self.description) == len(other.description)
 
     def __gt__(self, other):
-        return self.population > other.population
+        return len(self.description) > len(other.description)
 
     def __lt__(self, other):
-        return self.population < other.population
+        return len(self.description) < len(other.description)
 
 
-first_city = City("", "", "", "", "", "")
-second_city = City("", "", "", "", "", "")
+first_site = Website("", "", "")
+second_site = Website("", "", "",)
 
-first_city.create_city()
-second_city.create_city()
+first_site.create_site()
+second_site.create_site()
 
-print(first_city)
-print(second_city)
+print(first_site)
+print(second_site)
 
-print(first_city + 1000)
-print(second_city - 1000)
+print(first_site + "Some additional description")
+print(second_site - "second description")
 
-if first_city == second_city:
-    print("This cities have the same population")
-elif first_city > second_city:
-    print("First city is bigger than the second one")
-elif first_city < second_city:
-    print("Second city is bigger than the first one")
+if first_site == second_site:
+    print("This sites have the same description length")
+elif first_site > second_site:
+    print("First site description is longer than the second one")
+elif first_site < second_site:
+    print("Second site description is longer than the first one")
