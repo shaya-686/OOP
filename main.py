3a249b14ef871a2543b96ff3ef62e5782b1e7afe
shaya-686 <oleksandra.bogdanchenko@gmail.com>
2024-03-18 20:57:12 +0200

    Class Country with overloading operators

--- main.py
+++ main.py
@@ -24,115 +24,56 @@ class Country:
                 cities.append(city)
         self.cities = cities
 
-    def update_country(self, **kwargs):
-        for key, value in kwargs.items():
-            if key == "name":
-                self.name = value
-            elif key == "continent":
-                self.continent = value
-            elif key == "population":
-                self.population = value
-            elif key == "telephoneCode":
-                self.telephoneCode = value
-            elif key == "capital":
-                self.capital = value
-            else:
-                print("Unknown attribute")
-        self.display_country()
-
-    def add_city_to_country(self, city):
+    def __add__(self, city):
         if city in self.cities:
             print("City is already exist")
         else:
             self.cities.append(city)
             print(f"{city} is added to the {self.name}")
-            self.display_country()
+            return self
 
-    def delete_city_from_country(self, city):
+    def __sub__(self, city):
         if city in self.cities:
             self.cities.remove(city)
-            print("City is deleted")
-            self.display_country()
+            print("City is removed from the list")
+            return self
         else:
             print(f"{city} is not found in {self.name}")
 
-    def display_country(self):
-        print(f"Information about the country: "
-              f"name - {self.name}, "
-              f"continent - {self.continent}, "
-              f"population - {self.population},"
-              f"telephoneCode - {self.telephoneCode},"
-              f"capital - {self.capital},"
-              f"cities - {self.cities}")
+    def __eq__(self, other):
+        return self.population == other.population
 
+    def __gt__(self, other):
+        return self.population > other.population
 
-class City:
-    def __init__(self, name, region, country, population, zipCode, telephoneCode):
-        self.name = name
-        self.region = region
-        self.country = country
-        self.population = population
-        self.zipCode = zipCode
-        self.telephoneCode = telephoneCode
+    def __lt__(self, other):
+        return self.population < other.population
 
-    def create_city(self):
-        print("Enter the information about the city: ")
-        self.name = input("Enter the city name: ")
-        self.region = input("Enter the city region: ")
-        self.country = input("Enter the city country: ")
-        self.population = input("Enter the city population: ")
-        self.zipCode = input("Enter the city zipCode: ")
-        self.telephoneCode = input("Enter the city telephoneCode: ")
-
-    def display_city(self):
-        print(f"Information about the city: "
-              f"name - {self.name}, "
-              f"region - {self.region}, "
-              f"population - {self.population},"
-              f"telephoneCode - {self.telephoneCode},"
-              f"country - {self.country},"
-              f"zipCode - {self.zipCode}")
-
-
-class Human:
-    def __init__(self, name, birthday, phone, city, country, address):
-        self.name = name
-        self.birthday = birthday
-        self.phone = phone
-        self.city = city
-        self.country = country
-        self.address = address
-
-    def create_human(self):
-        print("Enter the information about the human: ")
-        self.name = input("Enter the human name: ")
-        self.birthday = input("Enter the human birthday: ")
-        self.phone = input("Enter the human phone: ")
-        self.city = input("Enter the human city: ")
-        self.country = input("Enter the human country: ")
-        self.address = input("Enter the human address: ")
-
-    def display_human(self):
-        print(f"Information about the human: "
-              f"name - {self.name}, "
-              f"birthday - {self.birthday}, "
-              f"phone - {self.phone},"
-              f"city - {self.city},"
-              f"country - {self.country},"
-              f"address - {self.address}")
+    def __str__(self):
+        return (f"Information about the country: "
+                f"name - {self.name}, "
+                f"continent - {self.continent}, "
+                f"population - {self.population},"
+                f"telephoneCode - {self.telephoneCode},"
+                f"capital - {self.capital},"
+                f"cities - {self.cities}")
 
 
 first_country = Country("", "", "", "", "", "")
+second_country = Country("", "", "", "", "", "")
+
 first_country.create_country()
-first_country.display_country()
-first_country.update_country(population=111111, telephoneCode=111)
-first_country.add_city_to_country("Kyiv")
-first_country.delete_city_from_country("Kyiv")
+second_country.create_country()
 
-first_city = City("", "", "", "", "", "")
-first_city.create_city()
-first_city.display_city()
+print(first_country)
+print(second_country)
 
-first_human = Human("", "", "", "", "", "")
-first_human.create_human()
-first_human.display_human()
+print(first_country + "Kyiv")
+print(first_country - "Kyiv")
+
+if first_country == second_country:
+    print("This countries have the same population")
+elif first_country > second_country:
+    print("First country is bigger than the second one")
+elif first_country < second_country:
+    print("Second country is bigger than the first one")
