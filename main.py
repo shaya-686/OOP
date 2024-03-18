eae4d63c88802b8933892faf145056ed7b9c2fef
shaya-686 <oleksandra.bogdanchenko@gmail.com>
2024-03-18 20:35:22 +0200

    Class Human with overloading operators

--- main.py
+++ main.py
@@ -1,97 +1,4 @@
-class Country:
-    def __init__(self, name, continent, population, telephoneCode, capital, cities):
-        self.name = name
-        self.continent = continent
-        self.population = population
-        self.telephoneCode = telephoneCode
-        self.capital = capital
-        self.cities = cities
-
-    def create_country(self):
-        print("Enter the information about the country: ")
-        self.name = input("Enter the country name: ")
-        self.continent = input("Enter the country continent: ")
-        self.population = input("Enter the country population: ")
-        self.telephoneCode = input("Enter the country telephone code: ")
-        self.capital = input("Enter the capital of the country: ")
-        cities = []
-        print("Add the city or press ENTER to break: ")
-        while True:
-            city = input("Enter the name of the city: ")
-            if city == "":
-                break
-            else:
-                cities.append(city)
-        self.cities = cities
-
-    def update_country(self, **kwargs):
-        for key, value in kwargs.items():
-            if key == "name":
-                self.name = value
-            elif key == "continent":
-                self.continent = value
-            elif key == "population":
-                self.population = value
-            elif key == "telephoneCode":
-                self.telephoneCode = value
-            elif key == "capital":
-                self.capital = value
-            else:
-                print("Unknown attribute")
-        self.display_country()
-
-    def add_city_to_country(self, city):
-        if city in self.cities:
-            print("City is already exist")
-        else:
-            self.cities.append(city)
-            print(f"{city} is added to the {self.name}")
-            self.display_country()
-
-    def delete_city_from_country(self, city):
-        if city in self.cities:
-            self.cities.remove(city)
-            print("City is deleted")
-            self.display_country()
-        else:
-            print(f"{city} is not found in {self.name}")
-
-    def display_country(self):
-        print(f"Information about the country: "
-              f"name - {self.name}, "
-              f"continent - {self.continent}, "
-              f"population - {self.population},"
-              f"telephoneCode - {self.telephoneCode},"
-              f"capital - {self.capital},"
-              f"cities - {self.cities}")
-
-
-class City:
-    def __init__(self, name, region, country, population, zipCode, telephoneCode):
-        self.name = name
-        self.region = region
-        self.country = country
-        self.population = population
-        self.zipCode = zipCode
-        self.telephoneCode = telephoneCode
-
-    def create_city(self):
-        print("Enter the information about the city: ")
-        self.name = input("Enter the city name: ")
-        self.region = input("Enter the city region: ")
-        self.country = input("Enter the city country: ")
-        self.population = input("Enter the city population: ")
-        self.zipCode = input("Enter the city zipCode: ")
-        self.telephoneCode = input("Enter the city telephoneCode: ")
-
-    def display_city(self):
-        print(f"Information about the city: "
-              f"name - {self.name}, "
-              f"region - {self.region}, "
-              f"population - {self.population},"
-              f"telephoneCode - {self.telephoneCode},"
-              f"country - {self.country},"
-              f"zipCode - {self.zipCode}")
+from datetime import datetime
 
 
 class Human:
@@ -106,33 +13,58 @@ class Human:
     def create_human(self):
         print("Enter the information about the human: ")
         self.name = input("Enter the human name: ")
-        self.birthday = input("Enter the human birthday: ")
+        while True:
+            date_format = "%Y/%m/%d"
+            date_of_birth = input("Enter the human birthday: ")
+            try:
+                self.birthday = datetime.strptime(date_of_birth, date_format)
+                break
+            except ValueError as e:
+                print("Date has incorrect type: ", e)
         self.phone = input("Enter the human phone: ")
         self.city = input("Enter the human city: ")
         self.country = input("Enter the human country: ")
         self.address = input("Enter the human address: ")
 
-    def display_human(self):
-        print(f"Information about the human: "
-              f"name - {self.name}, "
-              f"birthday - {self.birthday}, "
-              f"phone - {self.phone},"
-              f"city - {self.city},"
-              f"country - {self.country},"
-              f"address - {self.address}")
+    def __str__(self):
+        return (f"Human information: "
+                f"name - {self.name}, "
+                f"birthday - {self.birthday},"
+                f"phone - {self.phone},"
+                f"city - {self.city},"
+                f"country - {self.country},"
+                f"address - {self.address}")
 
+    def __sub__(self, other):
+        if self.birthday < other.birthday:
+            self.birthday, other.birthday = other.birthday, self.birthday
+        return self.birthday - other.birthday
 
-first_country = Country("", "", "", "", "", "")
-first_country.create_country()
-first_country.display_country()
-first_country.update_country(population=111111, telephoneCode=111)
-first_country.add_city_to_country("Kyiv")
-first_country.delete_city_from_country("Kyiv")
+    def __eq__(self, other):
+        return self.birthday == other.birthday
 
-first_city = City("", "", "", "", "", "")
-first_city.create_city()
-first_city.display_city()
+    def __gt__(self, other):
+        return self.birthday > other.birthday
+
+    def __lt__(self, other):
+        return self.birthday < other.birthday
+
 
 first_human = Human("", "", "", "", "", "")
+second_human = Human("", "", "", "", "", "")
+
 first_human.create_human()
-first_human.display_human()
+second_human.create_human()
+
+print(first_human)
+print(second_human)
+
+print("The difference between dates of birthday: ", first_human - second_human)
+
+if first_human == second_human:
+    print("This humans have birthdays in one day!")
+elif first_human < second_human:
+    print("First human is older than the second one")
+elif first_human > second_human:
+    print("Second human is older than the first one")
+
