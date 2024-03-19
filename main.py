aa62787f9d9f014357b8ab8ced4c03e8a84ee8b7
shaya-686 <oleksandra.bogdanchenko@gmail.com>
2024-03-19 19:24:13 +0200

    Class BankAccount with private attributes

--- main.py
+++ main.py
@@ -1,73 +1,37 @@
-class Clock:
-    def __init__(self, model, brand, year, price, clockType):
-        self.model = model
-        self.brand = brand
-        self.year = year
-        self.price = price
-        self.clockType = clockType
+class BankAccount:
+    def __init__(self, balance):
+        self.__balance = balance
 
-    def create_clock(self):
-        print("Enter the information about the clock: ")
-        self.model = input("Enter the clock model: ")
-        self.brand = input("Enter the clock brand: ")
-        self.clockType = input("Enter the clockType: ")
-        while True:
-            try:
-                self.year = int(input("Enter the clock year: "))
-                break
-            except ValueError as e:
-                print("Year should be int")
-        while True:
-            try:
-                self.price = float(input("Enter the clock price: "))
-                break
-            except ValueError as e:
-                print("Price should be float")
+    def get_balance(self):
+        return self.__balance
 
-    def __str__(self):
-        return (f"Information about the clock: "
-                f"model - {self.model}, "
-                f"brand - {self.brand}, "
-                f"year - {self.year},"
-                f"price - {self.price},"
-                f"clockType - {self.clockType}")
+    def deposit(self, amount):
+        """
+        Add some amount on balance
+        :param amount: (int, float) - addition amount of money
+        :return: None
+        """
+        self.__balance += amount
 
-    def __add__(self, some_price):
-        self.price += some_price
-        return self
-
-    def __sub__(self, some_price):
-        if some_price > self.price:
-            print("Price cannot be changed")
+    def withdraw(self, amount):
+        if amount < self.__balance:
+            self.__balance -= amount
         else:
-            self.price -= some_price
-            return self
-
-    def __eq__(self, other):
-        return self.price == other.price
-
-    def __gt__(self, other):
-        return self.price > other.price
-
-    def __lt__(self, other):
-        return self.price < other.price
+            print("Not enough balance")
 
 
-first_clock = Clock("", "", "", "", "")
-second_clock = Clock("", "", "", "", "")
+account = BankAccount(100)
+print(account.get_balance())
 
-first_clock.create_clock()
-second_clock.create_clock()
+account.deposit(55)
+print(account.get_balance())
 
-print(first_clock)
-print(second_clock)
+account.withdraw(60)
+print(account.get_balance())
 
-print(first_clock + 1000)
-print(second_clock - 1000)
+account.withdraw(200)
+print(account.get_balance())
 
-if first_clock == second_clock:
-    print("This clocks have the same price")
-elif first_clock > second_clock:
-    print("First clock is more expensive than the second one")
-elif first_clock < second_clock:
-    print("Second clock is more expensive than the first one")
+account.__balance
+
+
