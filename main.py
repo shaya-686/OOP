7f5a195463da9593c6e664228e42dd38afbdc04c
shaya-686 <oleksandra.bogdanchenko@gmail.com>
2024-03-20 22:16:48 +0200

    Create class Taxi for OOP lesson 3 homework

--- main.py
+++ main.py
@@ -1,44 +1,38 @@
-class Order:
-    roomTypes = {"Economy": 100, "Standard": 200, "Vip": 300}
-    minDays = 1
-    maxDays = 14
+class Taxi:
+    carTypes = {"Economy": 100, "Standard": 200, "Comfort": 300}
 
     def __init__(self):
         self.__client = None
-        self.__room = None
-        self.__days = None
+        self.__carType = None
+        self.__address = None
         self.__cost = None
 
     def get_client(self):
         return self.__client
 
-    def get_room(self):
-        return self.__room
+    def get_car_type(self):
+        return self.__carType
 
-    def get_days(self):
-        return self.__days
+    def get_address(self):
+        return self.__address
 
     def get_cost(self):
         return self.__cost
 
-    def set_room(self, new_room):
-        if str(new_room).capitalize() not in Order.roomTypes.keys():
-            print(f"You should choose the room type from the list: {Order.roomTypes}")
+    def set_car_type(self, new_car_type):
+        if str(new_car_type).capitalize() not in Taxi.carTypes.keys():
+            print(f"You should choose the carType type from the list: {Taxi.carTypes}")
         else:
-            self.__room = new_room.capitalize()
-            self.__cost = self.calculate_cost()
+            self.__carType = new_car_type.capitalize()
+            self.__cost = Taxi.carTypes[self.__carType]
 
-    def set_days(self, new_days):
-        if new_days in range(Order.minDays, Order.maxDays + 1):
-            self.__days = new_days
-            self.__cost = self.calculate_cost()
-        else:
-            print("Choose the correct days number from 1 to 14")
+    def set_address(self, new_address):
+        self.__address = new_address
 
     def __str__(self):
-        return f"Client: {self.__client}, room type: {self.__room}, days: {self.__days}, cost: {self.__cost}"
+        return f"Client: {self.__client}, car type: {self.__carType}, address: {self.__address}, cost: {self.__cost}"
 
-    def add_order(self):
+    def add_taxi(self):
         while True:
             try:
                 self.__client = input("Enter the client name: ").capitalize()
@@ -51,47 +45,42 @@ class Order:
 
         while True:
             try:
-                self.__room = input("Enter the room type: ").capitalize()
-                if self.__room not in Order.roomTypes.keys():
-                    raise ValueError(f"Choose room type from the existing values {Order.roomTypes.keys()}")
+                self.__carType = input("Enter the car type: ").capitalize()
+                if self.__carType not in Taxi.carTypes.keys():
+                    raise ValueError(f"Choose car type from the existing values {Taxi.carTypes.keys()}")
                 else:
                     break
             except ValueError as e:
                 print("Message: ", e)
         while True:
             try:
-                self.__days = int(input("Enter the number of days: "))
-                if self.__days not in range(Order.minDays, Order.maxDays + 1):
-                    raise ValueError(f"Choose the correct number of days from {Order.minDays} to {Order.maxDays}")
+                self.__address = input("Enter the client address: ")
+                if len(self.__address) == 0:
+                    raise ValueError(f"The number of symbols for client address should be bigger than 0")
                 else:
                     break
             except ValueError as e:
                 print("Message: ", e)
-        self.__cost = self.calculate_cost()
-
-    def calculate_cost(self):
-        return Order.roomTypes[self.__room] * self.__days
+        self.__cost = Taxi.carTypes[self.__carType]
 
     def __del__(self):
-        print("Order deleted!")
+        print("Taxi deleted!")
         del self
 
 
-order = Order()
-print(order)
-order.add_order()
+taxi = Taxi()
+print(taxi)
+taxi.add_taxi()
 
-print(f"{order.get_client()=}")
-print(f"{order.get_room()=}")
-print(f"{order.get_days()=}")
-print(f"{order.get_cost()=}")
+print(f"{taxi.get_client()=}")
+print(f"{taxi.get_car_type()=}")
+print(f"{taxi.get_address()=}")
+print(f"{taxi.get_cost()=}")
 
-order.set_room("Standard")
-print(order)
+taxi.set_car_type("Standard")
+print(taxi)
 
-order.set_days(7)
-print(order)
+taxi.set_address("New address")
+print(taxi)
 
-del order
-
-
+del taxi
