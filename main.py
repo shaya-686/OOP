9867d1a2b43951b109fc7f4efe43148716d7d19d
shaya-686 <oleksandra.bogdanchenko@gmail.com>
2024-03-20 21:39:21 +0200

    Class Order for OOP lesson 3 homework

--- main.py
+++ main.py
@@ -1,148 +1,97 @@
-class User:
-    def __init__(self, name, age, email):
-        self.__name = name
-        self.__age = age
-        self.__email = email
+class Order:
+    roomTypes = {"Economy": 100, "Standard": 200, "Vip": 300}
+    minDays = 1
+    maxDays = 14
 
-    def get_name(self):
-        return self.__name
-
-    def get_age(self):
-        return self.__age
-
-    def get_email(self):
-        return self.__email
-
-    def set_name(self, new_name):
-        self.__name = new_name
-
-    def set_age(self, new_age):
-        self.__age = new_age
-
-    def set_email(self, new_email):
-        self.__email = new_email
-
-    def __str__(self):
-        return f"Name: {self.__name}, date: {self.__age}, desc: {self.__email}"
-
-
-user = User("Bob", 30, "bob@gmail.com")
-print(f"{user.get_name()=}")
-print(f"{user.get_age()=}")
-print(f"{user.get_email()=}")
-
-user.set_age(10)
-user.set_name("Ann")
-user.set_email("new@gmail.com")
-print(user)
-
-
-class ShoppingCart:
     def __init__(self):
-        self.__products = []
+        self.__client = None
+        self.__room = None
+        self.__days = None
+        self.__cost = None
 
-    def get_products(self):
-        return self.__products
+    def get_client(self):
+        return self.__client
 
-    def add_product(self, product, price):
-        product_exists = False
-        for item in self.__products:
-            if product == item[0]:
-                print("Product already exists")
-                product_exists = True
-                break
-        if not product_exists:
-            self.__products.append((product, price))
+    def get_room(self):
+        return self.__room
 
-    def basket_total(self):
-        total = 0
-        for item in self.__products:
-            total += item[1]
-        return total
+    def get_days(self):
+        return self.__days
 
-    def __str__(self):
-        return f"{self.__products}"
+    def get_cost(self):
+        return self.__cost
 
-
-cart = ShoppingCart()
-cart.add_product("Milk", 200)
-cart.add_product("Coffee", 300)
-cart.add_product("Tea", 400)
-cart.add_product("Coffee", 300)
-
-print(f"{cart.get_products()=}")
-
-print(f"{cart.basket_total()=}")
-
-
-class Wallet:
-    def __init__(self, balance):
-        self.__balance = balance
-
-    def get_balance(self):
-        return self.__balance
-
-    def top_up(self, amount):
-        self.__balance += amount
-
-    def withdraw(self, amount):
-        if amount < self.__balance:
-            self.__balance -= amount
+    def set_room(self, new_room):
+        if str(new_room).capitalize() not in Order.roomTypes.keys():
+            print(f"You should choose the room type from the list: {Order.roomTypes}")
         else:
-            print("Not enough balance")
+            self.__room = new_room.capitalize()
+            self.__cost = self.calculate_cost()
 
-
-wallet = Wallet(50)
-print(f"{wallet.get_balance()=}")
-
-wallet.top_up(50)
-print(f"{wallet.get_balance()=}")
-
-wallet.withdraw(60)
-print(f"{wallet.get_balance()=}")
-
-wallet.withdraw(200)
-print(f"{wallet.get_balance()=}")
-
-
-class Computer:
-    def __init__(self, processor, ram, gpu):
-        self.__processor = processor
-        self.__ram = ram
-        self.__gpu = gpu
-
-    def get_processor(self):
-        return self.__processor
-
-    def get_ram(self):
-        return self.__ram
-
-    def get_gpu(self):
-        return self.__gpu
-
-    def set_processor(self, new_processor):
-        self.__processor = new_processor
-
-    def set_ram(self, new_ram):
-        self.__ram = new_ram
-
-    def set_gpu(self, new_gpu):
-        self.__gpu = new_gpu
+    def set_days(self, new_days):
+        if new_days in range(Order.minDays, Order.maxDays + 1):
+            self.__days = new_days
+            self.__cost = self.calculate_cost()
+        else:
+            print("Choose the correct days number from 1 to 14")
 
     def __str__(self):
-        return f"Processor: {self.__processor}, ram: {self.__ram}, video card: {self.__gpu}"
+        return f"Client: {self.__client}, room type: {self.__room}, days: {self.__days}, cost: {self.__cost}"
+
+    def add_order(self):
+        while True:
+            try:
+                self.__client = input("Enter the client name: ").capitalize()
+                if len(self.__client) == 0:
+                    raise ValueError(f"The number of symbols for client name should be bigger than 0")
+                else:
+                    break
+            except ValueError as e:
+                print("Message: ", e)
+
+        while True:
+            try:
+                self.__room = input("Enter the room type: ").capitalize()
+                if self.__room not in Order.roomTypes.keys():
+                    raise ValueError(f"Choose room type from the existing values {Order.roomTypes.keys()}")
+                else:
+                    break
+            except ValueError as e:
+                print("Message: ", e)
+        while True:
+            try:
+                self.__days = int(input("Enter the number of days: "))
+                if self.__days not in range(Order.minDays, Order.maxDays + 1):
+                    raise ValueError(f"Choose the correct number of days from {Order.minDays} to {Order.maxDays}")
+                else:
+                    break
+            except ValueError as e:
+                print("Message: ", e)
+        self.__cost = self.calculate_cost()
+
+    def calculate_cost(self):
+        return Order.roomTypes[self.__room] * self.__days
+
+    def __del__(self):
+        print("Order deleted!")
+        del self
 
 
-computer = Computer("AMD Ryzen 7 4800H (2.9 - 4.2 ГГц)", 8, "nVidia GeForce RTX 3050")
-print(f"{computer.get_processor()=}")
-print(f"{computer.get_ram()=}")
-print(f"{computer.get_gpu()=}")
+order = Order()
+print(order)
+order.add_order()
 
-computer.set_processor("Intel Core i5-10400H (2.6 - 4.6 ГГц)")
-print(computer)
+print(f"{order.get_client()=}")
+print(f"{order.get_room()=}")
+print(f"{order.get_days()=}")
+print(f"{order.get_cost()=}")
 
-computer.set_ram(16)
-print(computer)
+order.set_room("Standard")
+print(order)
 
-computer.set_gpu("nVidia GeForce GTX 1650")
-print(computer)
+order.set_days(7)
+print(order)
+
+del order
+
+
